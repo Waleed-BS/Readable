e76a1fc3a3687085381f51096a4b394fa207d404
Waleed-BS <waleed-bs@live.com>
2017-09-06 21:50:21 -0400

    feat: add Actions
    Added action creators for posts, comments, and categories.

---
+++ readable/src/actions/Comments_Actions.js
@@ -0,0 +1,32 @@
+export const GET_COMMENTS = 'GET_COMMENTS'
+export const ADD_COMMENT = 'ADD_COMMENT'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+export const EDIT_COMMENT = 'EDIT_COMMENT'
+
+export function getComments ( {comments} ) {
+  return {
+    type: GET_COMMENTS,
+    comments
+  }
+}
+
+export function addComment ( {comment} ) {
+  return {
+    type: ADD_COMMENT,
+    comment
+  }
+}
+
+export function deleteComment ( {commentId} ) {
+  return {
+    type: DELETE_COMMENT,
+    commentId
+  }
+}
+
+export function editComment ( {comment} ) {
+  return {
+    type: EDIT_COMMENT,
+    comment
+  }
+}
---
+++ readable/src/actions/Posts_Actions.js
@@ -0,0 +1,24 @@
+export const GET_POSTS = 'GET_POSTS'
+export const DELETE_POST = 'DELETE_POST'
+export const EDIT_POST = 'EDIT_POST'
+
+export function getPosts ( {posts} ) {
+  return {
+    type: GET_POSTS,
+    posts
+  }
+}
+
+export function deletePost ( {postId} ) {
+  return {
+    type: DELETE_POST,
+    postId
+  }
+}
+
+export function editPost ( {post} ) {
+  return {
+    type: EDIT_POST,
+    post
+  }
+}
